8c19b32ef122b4cafcf6c024c061e7ae9c4cdedc
annikahudak <annikahudak@yahoo.com>
2017-03-04 22:04:58 -0800

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace Sudoku
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.fileNametb = new System.Windows.Forms.TextBox();
+            this.goBtn = new System.Windows.Forms.Button();
+            this.filename_label = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // fileNametb
+            // 
+            this.fileNametb.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.fileNametb.Location = new System.Drawing.Point(126, 95);
+            this.fileNametb.Name = "fileNametb";
+            this.fileNametb.Size = new System.Drawing.Size(201, 20);
+            this.fileNametb.TabIndex = 0;
+            this.fileNametb.Text = "ex: \"test1.csv\"";
+            this.fileNametb.MouseClick += new System.Windows.Forms.MouseEventHandler(this.fileNametb_MouseClick);
+            this.fileNametb.TextChanged += new System.EventHandler(this.fileNametb_TextChanged);
+            // 
+            // goBtn
+            // 
+            this.goBtn.Location = new System.Drawing.Point(354, 91);
+            this.goBtn.Name = "goBtn";
+            this.goBtn.Size = new System.Drawing.Size(75, 23);
+            this.goBtn.TabIndex = 1;
+            this.goBtn.Text = "Go";
+            this.goBtn.UseVisualStyleBackColor = true;
+            this.goBtn.Click += new System.EventHandler(this.goBtn_Click);
+            // 
+            // filename_label
+            // 
+            this.filename_label.AutoSize = true;
+            this.filename_label.Location = new System.Drawing.Point(123, 68);
+            this.filename_label.Name = "filename_label";
+            this.filename_label.Size = new System.Drawing.Size(80, 13);
+            this.filename_label.TabIndex = 2;
+            this.filename_label.Text = "Enter file name:";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 21F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dataGridView1.DefaultCellStyle = dataGridViewCellStyle1;
+            this.dataGridView1.Location = new System.Drawing.Point(88, 55);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(293, 331);
+            this.dataGridView1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(568, 427);
+            this.Controls.Add(this.filename_label);
+            this.Controls.Add(this.goBtn);
+            this.Controls.Add(this.fileNametb);
+            this.Controls.Add(this.dataGridView1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Sudoku Validator";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox fileNametb;
+        private System.Windows.Forms.Button goBtn;
+        private System.Windows.Forms.Label filename_label;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace Sudoku
+{
+    public partial class Form1 : Form
+    {
+        int[,] grid = new int[9,9];
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.CenterToScreen();
+            dataGridView1.Hide();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        void init()
+        {
+            Console.WriteLine("Before start thread");
+
+            Thread[] testThread = new Thread[10];
+            Thread tid1 = new Thread(new ThreadStart(MyThread.Thread1));
+            Thread tid2 = new Thread(new ThreadStart(MyThread.Thread2));
+
+            testThread[0] = tid1;
+            testThread[1] = tid2;
+
+            for (int i = 0; i < 9; ++i)
+            {
+                Thread tid = new Thread(new ThreadStart(MyThread.Thread3));
+                testThread[i + 2] = tid;
+            }
+            foreach (Thread myThread in testThread)
+            {
+                myThread.Start();
+            }
+
+        }
+        void readFile()
+        {
+            string line;
+            var lines = new List<string[]>();
+
+            // Read the file and display it line by line.
+            System.IO.StreamReader file = new System.IO.StreamReader(fileNametb.Text);
+            while ((line = file.ReadLine()) != null)
+            {
+                string[] Line = line.Split(',');
+                lines.Add(Line);
+                //Console.WriteLine(line);
+
+            }
+
+            file.Close();
+            var data = lines.ToArray();
+            for (int i = 0; i < 9; ++i)
+            {
+                for (int j = 0; j < 9; ++j)
+                {
+                    grid[i, j] = Int32.Parse(data[i][j]);
+                }
+
+            }
+        }
+        void printGrid()
+        {
+
+            this.dataGridView1.ColumnCount = 9;
+
+            for (int x = 0; x < 9; x++)
+            {
+                DataGridViewRow row = new DataGridViewRow();
+                row.CreateCells(this.dataGridView1);
+
+                for (int y = 0; y < 9; y++)
+                {
+                    row.Cells[y].Value = grid[x, y];
+                }
+                this.dataGridView1.Rows.Add(row);
+            }
+            this.dataGridView1.ColumnHeadersVisible = false;
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.DisplayedCells;
+            this.dataGridView1.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.DisplayedCells;
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.RowsDefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleRight;
+           
+
+            dataGridView1.Show();
+       }
+
+        //Read file
+        private void goBtn_Click(object sender, EventArgs e)
+        {
+            readFile();
+            fileNametb.Hide();
+            goBtn.Hide();
+            filename_label.Hide();
+            printGrid();
+            init();
+        }
+
+        private void fileNametb_TextChanged(object sender, EventArgs e)
+        {
+          
+        }
+
+        private void fileNametb_MouseClick(object sender, MouseEventArgs e)
+        {
+            fileNametb.Text = " ";
+            fileNametb.ForeColor = Color.Black;
+        }
+    }
+    public class MyThread
+    {
+        public static void Thread1()
+        {
+            Console.WriteLine("Hello I am Row Thread");
+        }
+        public static void Thread2()
+        {
+            Console.WriteLine("Hello I am Column Thread");
+        }
+        public static void Thread3()
+        {
+            Console.WriteLine("Hello I am Square Thread");
+        }
+    }
+}
